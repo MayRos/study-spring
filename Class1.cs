86a7b7dd6df7940a85e9b1f14b12ef4ae8cecf8f
MayRos <3270728477>
2021-05-20 08:50:09 +0800

    learn how to create a C# with ui

---
+++ Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WpfApp1
+{
+    class Class1
+    {
+        public string text { set; get; }
+        
+        public void print()
+        {
+            Console.WriteLine(text);
+        }
+    }
+}
