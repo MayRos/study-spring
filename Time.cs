9d4fb6f78f74ba3c0c80b45785fdb6f219c9dc42
MayRos <3270728477@qq.com>
2021-06-02 11:26:44 +0800

    嗯嗯

---
+++ Time.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace WpfApp1
+{
+    class Time
+    {
+        Timer _timer ;
+        public Action Action { set; get; }
+        public int Period { get; set; }
+
+        public void Execute()
+        {
+            if(_timer != null)
+            {
+
+            }
+        }
+
+    }
+
+}
